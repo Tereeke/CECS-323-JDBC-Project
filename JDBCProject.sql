d432ecc2b637dcea134c0b05a0b26fe97f1cc203
Diego Gonzalez <dgonzalez21@users.noreply.github.com>
2018-10-23 15:22:29 -0700

    Update
    update

--- JDBCProject.sql
+++ JDBCProject.sql
@@ -39,14 +39,23 @@ ALTER TABLE books
 INSERT INTO writingGroups VALUES('Writers Annonymous','Elon Musk','2018','Fiction');
 INSERT INTO writingGroups VALUES('Awesome Writers','Steve Jobs','2013','Mystery');
 INSERT INTO writingGroups VALUES('Resounding Maybe','John Doe','2000','Science Fiction');
+INSERT INTO writingGroups VALUES('Cool Writers','Mike Doug','2013','Fantasy');
+INSERT INTO writingGroups VALUES( 'Maybe neato','John Doe','2000','Science Fiction');
 
 INSERT INTO publishers VALUES('Publishers R Us', '123 Writers St', '555-555-5555', 'pubRus@writing.com');
 INSERT INTO publishers VALUES('We Publish 4 U', '777 Lucky Lane', '777-777-7777', 'pub4hire@writing.com');
 INSERT INTO publishers VALUES('Publisher For You', '321 State St', '012-345-6789', 'publishers@gmail.com');
+INSERT INTO publishers VALUES('Nicks Books','123 street','2018','diego@booksnick.com');
+INSERT INTO publishers VALUES('Samis Books','323 street','2019','diego@samibook.com');
+INSERT INTO publishers VALUES('Steve Books','447 street','2030','diego@stevebook.com');
+INSERT INTO publishers VALUES('Greg Books','7878 street','2008','diego@diodoi.com');
 
 INSERT INTO books VALUES('To Mars and Back', '2018', '1200', 'Writers Annonymous', 'Publishers R Us');
 INSERT INTO books VALUES('Electric Cars', '2018', '1000', 'Writers Annonymous', 'Publishers R Us');
-INSERT INTO books VALUES('Star Trek Wars', '2005', '20000', 'Resounding Maybe', 'Publisher For You');
+INSERT INTO books VALUES('Star Trek Wars', '2005', '20000', '', 'Publisher For You');
+INSERT INTO books VALUES('Tesla Battle', '2017', '10', 'Writers Annonymous', 'Nicks Books');
+INSERT INTO books VALUES('Godzilla 7', '2003', '245', 'Cool Writers', 'Publisher For You');
+
 
 SELECT * FROM writingGroups;
 SELECT * FROM publishers;
